3ccf200b5804f5f73ab8c71fc3f6b4f637d80597
Haidar Dargaye <haidardargaye@gmail.com>
2019-10-01 06:54:13 +0400

    ship and bullet

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
 var canvas = document.getElementById("game-area");
 var ctx = canvas.getContext("2d");
-let ship1 = new Ship(0, canvas.height / 2, 1);
-let ship2 = new Ship(canvas.width, canvas.height / 2, -1);
-ship2.xPosition = canvas.width - ship2.width;
-
-
+let ship1 = new Ship(0, canvas.height / 2, 1, ctx);
+let ship2 = new Ship(canvas.width - SHIP_WIDTH, canvas.height / 2, -1, ctx);
 
 document.addEventListener("keydown", keydownHandler, false);
 
 function keydownHandler(e) {
-    if (e.key == "ArrowUp") {
-        ship1.moveUp(-10);
-    } else if (e.key == "ArrowDown") {
-        ship1.moveUp(10);
+    switch(e.key){
+        case "ArrowUp": ship1.moveUp(-10); break;
+        case "ArrowDown" : ship1.moveUp(10); break;
+        case " " : ship1.shoot() ; break;
+        default : break;
     }
 }
 
 let init = () => {
     ship1.draw(ctx);
     ship2.draw(ctx);
-    setInterval(gameLoop, 50);
+    setInterval(gameLoop, 200);
 }
 
 
 let gameLoop = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ship1.draw(ctx);
-    ship2.draw(ctx);
+    ship1.draw();
+    ship2.draw();
+    if (ship1.bullets && ship1.bullets.length > 0){
+        ship1.bullets.forEach((bullet, index, arr)=>{
+            if (bullet.collisionX(ship2.x) && bullet.collisionY(ship2)){
+                console.log("HIT SHIP 2");
+                arr.splice(index,1);
+                return;
+            }
+            if (bullet.collisionX(canvas.width)){
+                    arr.splice(index,1);
+            }
+        });
+    }
 }
 
 init();
--- ship.js
+++ ship.js
@@ -1,49 +1,71 @@
-function Ship(xPosition, yPosition, direction) {
-    this.xPosition = xPosition;
-    this.yPosition = yPosition;
-    this.width = 20;
-    this.height = 10;
+
+const SHIP_WIDTH = 20;
+const SHIP_HEIGHT = 10;
+const BULLET_WIDTH = 5;
+const BULLET_HEIGHT = 5;
+
+
+function Ship(x, y, direction, ctx) {
+    this.x = x;
+    this.y = y;
+    this.width = SHIP_WIDTH;
+    this.height = SHIP_HEIGHT;
     this.color = "#0095DD";
     this.direction = direction;
+    this.ctx = ctx;
+    this.bullets = [];
 }
 
-Ship.prototype.moveUp = function (yCoordinate) {
-    this.yPosition += yCoordinate;
+Ship.prototype.moveUp = function (yDisp) {
+    this.y += yDisp;
 }
 
-Ship.prototype.draw = function (ctx) {
-    ctx.beginPath();
-    ctx.rect(this.xPosition, this.yPosition, this.width, this.height);
-    ctx.fillStyle = this.color;
-    ctx.fill();
-    ctx.closePath();
+Ship.prototype.draw = function () {
+    this.ctx.beginPath();
+    this.ctx.rect(this.x, this.y, SHIP_WIDTH, SHIP_HEIGHT);
+    this.ctx.fillStyle = this.color;
+    this.ctx.fill();
+    this.ctx.closePath();
+
+    if (this.bullets && this.bullets.length > 0) {
+        this.bullets.forEach((bullet) => {
+            bullet.moveX(10 * this.direction);
+            bullet.draw();
+        });
+    }
 }
 
-Ship.prototype.shoot = function (anotherShip) {
-    let bullet = new Bullet(this.xPosition, this.yPosition);
+Ship.prototype.shoot = function () {
+    let bullet = new Bullet(this.x + SHIP_WIDTH, this.y + SHIP_HEIGHT / 2 - BULLET_HEIGHT / 2, this.ctx);
+    this.bullets.push(bullet);
 }
 
-Ship.prototype.collision = function (anotherShip, bullet){
-
-}
-
-
-function Bullet(xPosition, yPosition) {
-    this.xPosition = xPosition;
-    this.yPosition = yPosition;
-    this.width = 5;
-    this.height = 5;
+function Bullet(x, y, ctx) {
+    this.x = x;
+    this.y = y;
     this.color = "#0095DD";
+    this.ctx = ctx;
+    this.width = BULLET_WIDTH;
+    this.height = BULLET_HEIGHT;
 }
 
-Bullet.prototype.shoot = function (xCoordinate) {
-    this.xPosition += xCoordinate;
+Bullet.prototype.moveX = function (xDisp) {
+    this.x += xDisp;
 }
 
-Bullet.prototype.draw = function(ctx){
-    ctx.beginPath();
-    ctx.rect(this.xPosition, this.yPosition, this.width, this.height);
-    ctx.fillStyle = this.color;
-    ctx.fill();
-    ctx.closePath();
+Bullet.prototype.draw = function () {
+    this.ctx.beginPath();
+    this.ctx.rect(this.x, this.y, BULLET_WIDTH, BULLET_HEIGHT);
+    this.ctx.fillStyle = this.color;
+    this.ctx.fill();
+    this.ctx.closePath();
+}
+
+
+Bullet.prototype.collisionX = function (boundaryX) {
+    return (this.x + BULLET_WIDTH >= boundaryX);
+}
+
+Bullet.prototype.collisionY = function (shape){
+    return (this.y + BULLET_HEIGHT <= shape.y + shape.height) && (this.y >= shape.y);
 }
